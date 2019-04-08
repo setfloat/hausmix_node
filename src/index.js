a6f285e2bab52b67ed74a4ee841e588b198781a6
Patrick Richardson <setfloat@gmail.com>
2019-04-08 16:23:39 -0700

    basic node/graphql template with user signup/signin/signout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
-// require("dotenv");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 require("dotenv").config({ path: ".env" });
-
 const createServer = require("./createServer");
 const db = require("./db");
 
 const server = createServer();
 
 // TODO Use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
+
 // TODO Use express middleware to populate current User
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+
+  next();
+});
+
+server.express.use(async (req, res, next) => {
+  if (!req.userId) return next();
+  const user = await db.query
+    .user(
+      { where: { id: req.userId } },
+      "{ id, permissions, email, name, households { id }, householdsManaged  { id }, createdChores  { id }, assigned  { id }, currentAssigned  { id } }"
+    )
+    .catch(err => console.log(err));
+
+  console.log({ user });
+  req.user = user;
+  next();
+});
 
 server.start(
   {
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -19,11 +19,35 @@ const Mutation = {
     });
   },
 
-  async signup(parent, args, ctx, info) {
+  async signIn(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+
+    const user = await ctx.db.query.user({
+      where: { email: args.email }
+    });
+    if (!user) {
+      throw new Error(`No such user found for ${args.email}`);
+    }
+    const valid = await bcrypt.compare(args.password, user.password);
+    if (!valid) {
+      throw new Error("Invalid Password");
+    }
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    delete args.password;
+
+    return user;
+  },
+
+  async signUp(parent, args, ctx, info) {
     args.email = args.email.toLowerCase();
 
     const password = await bcrypt.hash(args.password, 14);
-    // delete args.password;
+    delete args.password;
 
     const user = await ctx.db.mutation.createUser(
       {
@@ -48,6 +72,10 @@ const Mutation = {
     });
 
     return user;
+  },
+  signOut(parent, args, ctx, info) {
+    ctx.response.clearCookie("token");
+    return { message: "Successfully signed out. Thanks for stopping by!" };
   }
 };
 
